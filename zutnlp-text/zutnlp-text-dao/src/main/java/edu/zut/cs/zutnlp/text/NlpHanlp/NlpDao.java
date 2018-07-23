3ff720e37d82fd768606a5781c81df5774d8a14c
memetuanti <z347675616@foxmail.com>
2018-07-23 15:43:39 +0800

    add zutnlp_text Framework and logs

---
+++ zutnlp-text/zutnlp-text-dao/src/main/java/edu/zut/cs/zutnlp/text/NlpHanlp/NlpDao.java
@@ -0,0 +1,8 @@
+package edu.zut.cs.zutnlp.text.NlpHanlp;
+
+import edu.zut.cs.zutnlp.base.dao.GenericDao;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface NlpDao extends GenericDao<NlpHanlp,Long> {
+}
