1c488200444c4f5d6f93cebb3a235a385281222c
memetuanti <z347675616@foxmail.com>
2018-07-24 09:48:25 +0800

    delete dir

--- zutnlp-master/zutnlp-text/zutnlp-text-dao/src/main/java/edu/zut/cs/zutnlp/text/grammar/dao/grarecognitionDao.java
+++
@@ -1,9 +0,0 @@
-package edu.zut.cs.zutnlp.text.grammar.dao;
-
-import edu.zut.cs.zutnlp.base.dao.GenericDao;
-import edu.zut.cs.zutnlp.text.grammar.domain.grarecognition;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface grarecognitionDao extends GenericDao<grarecognition,Long> {
-}
--- 新建文件夹/zutnlp-master/zutnlp-text/zutnlp-text-dao/src/main/java/edu/zut/cs/zutnlp/text/NlpHanlp/NlpDao.java
+++
@@ -1,8 +0,0 @@
-package edu.zut.cs.zutnlp.text.NlpHanlp;
-
-import edu.zut.cs.zutnlp.base.dao.GenericDao;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface NlpDao extends GenericDao<NlpHanlp,Long> {
-}
